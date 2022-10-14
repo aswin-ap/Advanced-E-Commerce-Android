c35a191e403e2101a9756f539ea0f4e8125ec424
Aswin <aswinap99@gmail.com>
2022-10-14 20:57:56 +0530

    LoginFragment

--- app/src/main/java/com/example/pos_android/view/auth/AuthActivity.java
+++ app/src/main/java/com/example/pos_android/view/auth/AuthActivity.java
@@ -1,16 +1,79 @@
 package com.example.pos_android.view.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.pos_android.R;
+import com.example.pos_android.databinding.ActivityAuthBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class AuthActivity extends AppCompatActivity {
+    Fragment currentFragment;
+    private ActivityAuthBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+        binding = ActivityAuthBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initView();
+    }
+
+    private void initView() {
+        getSupportActionBar().hide();
+
+        TabLayout.Tab loginTab = binding.tabLayout.newTab();
+        loginTab.setText("Login");
+        TabLayout.Tab signupTab = binding.tabLayout.newTab();
+        signupTab.setText("Sign Up");
+        binding.tabLayout.addTab(loginTab);
+        binding.tabLayout.addTab(signupTab);
+        openFragment(getCurrentFragment(0));
+
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                currentFragment = null;
+                currentFragment = getCurrentFragment(tab.getPosition());
+                openFragment(currentFragment);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    Fragment getCurrentFragment(int position) {
+        switch (position) {
+            case 0:
+                return new LoginFragment();
+
+            case 1:
+                return new SignupFragment();
+
+            default:
+                return null;
+
+        }
+    }
+
+    private void openFragment(final Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 }
