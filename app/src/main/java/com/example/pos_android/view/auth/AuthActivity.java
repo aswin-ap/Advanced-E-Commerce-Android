579d9a9f857fd797d6a4b291f4618a57c66e5f20
Aswin <aswin@pegasusone.com>
2022-10-14 18:13:38 +0530

    SplashActivity

---
+++ app/src/main/java/com/example/pos_android/view/auth/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.pos_android.view.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pos_android.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
